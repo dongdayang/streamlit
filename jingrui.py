a399cffcc8d9a817aa93566d8373dc5ad8dc2cb6
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-05-08 15:29:10 +0800

    Update jingrui.py

--- jingrui.py
+++ jingrui.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import pydeck as pdk
-
+import matplotlib.pyplot as plt
 
 # SETTING PAGE CONFIG TO WIDE MODE
 st.set_page_config(layout="wide")
@@ -15,10 +15,9 @@ DATA_URL = (
 
 
 @st.cache(persist=True)
-
-def load_data(lat, lon, district, state):
+def load_mapdata(lat, lon, district, state):
     df0 = pd.read_excel(DATA_URL,
-                        sheet_name=state)
+                        sheet_name=state, )
     count = df0['组织区域'][df0['组织区域'] == district].count()
 
     df1 = pd.DataFrame(
@@ -27,13 +26,47 @@ def load_data(lat, lon, district, state):
     return df1
 
 
+def load_data(district, state):
+    df0 = pd.read_excel(DATA_URL,
+                        sheet_name=state, )
+    count = df0['组织区域'][df0['组织区域'] == district].value_counts()
+    return count
+
+
 df = pd.concat([
-    load_data(31.181915, 121.589851, '上海一区', '入职'),
-    load_data(31.191915, 121.589851, '上海二区', '入职'),
-    load_data(31.22114, 121.54409, '上海三区', '入职'),
-    load_data(31.107929, 121.581902, '上海四区', '入职'),
-    load_data(31.171915, 121.589851, '上海五区', '入职'),
+    load_mapdata(31.181915, 121.589851, '上海一区', '入职'),
+    load_mapdata(31.191915, 121.589851, '上海二区', '入职'),
+    load_mapdata(31.22114, 121.54409, '上海三区', '入职'),
+    load_mapdata(31.107929, 121.581902, '上海四区', '入职'),
+    load_mapdata(31.171915, 121.589851, '上海五区', '入职'),
 ])
+
+df2 = pd.concat([
+    load_data('上海三区', '入职'),
+    load_data('上海四区', '入职'),
+    load_data('上海五区', '入职'),
+    load_data('上海六区', '入职'),
+])
+df3 = pd.concat([
+    load_data('上海三区', '离职'),
+    load_data('上海四区', '离职'),
+    load_data('上海五区', '离职'),
+    load_data('上海六区', '离职'),
+])
+print(df2)
+# df2.columns = ['入职', '离职']
+
+df4 = pd.DataFrame(df2)
+df4.columns = ['入职']
+df5 = pd.DataFrame(df3)
+df5.columns = ['离职']
+df4['离职'] = df5['离职']
+
+
+# print(df4)
+
+
+# print(df4)
 
 
 # CREATING FUNCTION FOR MAPS
@@ -64,5 +97,19 @@ def map(data, lat, lon, zoom):
     ))
 
 
-st.write("各区域招聘数据分布图")
-map(df, 31.23035, 121.473717, 10)
+st.write("各区域招聘分布图")
+
+row1_1, row1_2, row1_3 = st.beta_columns((2, 1, 1))
+
+with row1_1:
+    map(df, 31.23035, 121.473717, 10)
+
+with row1_2:
+    st.write("")
+
+with row1_3:
+    st.write("各大区招聘直方图")
+
+    chart_data = df4
+
+    st.bar_chart(chart_data)
