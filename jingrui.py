2a6caf12963c668f5fbdaf108d80b03c697dfee2
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-05-08 15:34:17 +0800

    Update jingrui.py

--- jingrui.py
+++ jingrui.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import pydeck as pdk
-import matplotlib.pyplot as plt
 
 # SETTING PAGE CONFIG TO WIDE MODE
 st.set_page_config(layout="wide")
